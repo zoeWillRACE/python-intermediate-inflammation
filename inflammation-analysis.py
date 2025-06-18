841d8c252f0a8625ab007a305b78aa389e0d2308
Zoe Williams <zoe.williams@ukaea.uk>
2025-06-18 14:43:19 +0100

    Updated to newer version of inflammation-analysis.py

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -17,19 +17,25 @@ def main(args):
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
-    in_files = args.infiles
-    if not isinstance(in_files, list):
-        in_files = [args.infiles]
+    infiles = args.infiles
+    if not isinstance(infiles, list):
+        infiles = [args.infiles]
 
-    for filename in in_files:
+
+    if args.full_data_analysis:
+        analyse_data(os.path.dirname(infiles[0]))
+        return
+
+    for filename in infiles:
         inflammation_data = models.load_csv(filename)
 
-    view_data = {
-                'average': models.daily_mean(inflammation_data),
-                'max': models.daily_max(inflammation_data),
-                'min': models.daily_min(inflammation_data)}
+        view_data = {
+            'average': models.daily_mean(inflammation_data),
+            'max': models.daily_max(inflammation_data),
+            'min': models.daily_min(inflammation_data)
+        }
 
-    views.visualize(view_data)
+        views.visualize(view_data)
 
 
 
